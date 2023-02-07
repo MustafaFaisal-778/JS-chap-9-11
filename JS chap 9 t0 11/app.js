e5212ca5cd71443707dd7a2d5ae8fa8b0e5c97bb
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-02-07 19:19:43 +0500

    Add files via upload

---
+++ JS chap 9 t0 11/app.js
@@ -0,0 +1,203 @@
+//Question#1
+// var city=prompt("Enter city name")
+
+// if(city=="karachi"){
+//     alert("Welcome to city of light")
+// }
+
+//Question#2
+// var gender=prompt("Enter your gender")
+// if(gender=="male"){
+//     alert("Good Morning Sir")
+// }
+// if(gender=="female"){
+//     alert("Good Morning Ma’am.")
+// }
+
+//Question#3
+// var color= prompt("Enter color")
+// if (color==="red"){
+//     alert("Must Stop")
+// }
+// if (color==="yellow"){
+//     alert("Ready to move")
+// }
+// if (color==="green"){
+//     alert("Move now")
+// }
+
+//Question4
+// var fuel=+prompt("Enter remaining fuel in car")
+// if (fuel< 0.25){
+//     alert("“Please refill the fuel in your car”")
+// }
+
+//Question5
+
+    //a
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// //yes it works
+
+// //b
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// //not working
+
+//c
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// //not works
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// //it works
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// //not works
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//works
+
+//d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //works
+
+//e
+// if (true){
+//     alert("True");
+//     }
+// if (false){
+//     alert("False");
+// }
+    // woks
+
+//f
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     } 
+// working   
+
+//Question#6
+// var subject1Marks=+prompt("Marks of 1st subject")
+// var subject2Marks=+prompt("Marks of 2nd subject")
+// var subject3Marks=+prompt("Marks of 3rd subject")
+
+// var totalMarks=+prompt("Total Marks")
+
+// var obtainMarks=+(subject1Marks+subject2Marks+subject3Marks)
+//  var percentage=+((obtainMarks*100)/totalMarks)
+
+// document.write("<h1>Marks Sheet</h1>")
+// document.write("<p>Total marks : "+totalMarks+"</p>")
+// document.write("<p>Marks obtain : "+obtainMarks+"</p>")
+// document.write("<p>Percentage : "+percentage+"</p>")
+
+// if(percentage>=80){
+    
+//     document.write("<p>Grade : A-one </p>")
+//     document.write("<p>Remarks : Excellent</p>")
+
+// }
+
+// if(percentage>=70 && percentage<=80){
+//     document.write("<p>Grade : A</p>")
+//     document.write("<p>Remarks : Good</p>")
+// }
+
+// if(percentage>=60 && percentage<=70){
+//     document.write("<p>Grade : B</p>")
+//     document.write("<p>Remarks : You need to improve</p>")
+
+// }
+// if(percentage<=60){
+//     document.write("<p>Grade : fail</p>")
+//     document.write("<p>Remarks : sorry</p>")
+    
+// }
+
+//Question7
+// var secretNumber=8
+// var guess=+prompt("guess the number")
+ 
+// if(guess==secretNumber){
+//     alert("Bingo! correct answer")
+// }
+// if(guess==++secretNumber){
+//     alert("Close enough to the correct answer")
+// }
+// else{
+//     alert("wrong answer")
+// }
+
+//Question8
+// var num1=+prompt("Enter a number")
+// if(num1%3==0){
+//     alert("the number is divisible by 3")
+// }
+// else{
+//     alert("the number is not divisible by 3")
+// }
+
+//Question#9
+// var num1=+prompt("Enter a number")
+// if(num1%2==0){
+//     alert("the number is even")
+// }
+// else{
+//     alert("the number is odd")
+// }
+
+//Question#10
+// var t=+prompt("enter temperature")
+
+// if(t>=40){
+//     alert("It is too hot outside.")
+// }
+
+// else if(t>=30){
+//     alert("“The Weather today is Normal.”")
+// }
+// else if(t>=20){
+//     alert("“Today’s Weather is cool.”")
+// }
+// else if(t>=10){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+
+//Question#11
+// var  num1=+prompt("enter 1st number")
+// var  num2=+prompt("enter 2nd number")
+// var opration=prompt("enter + - * / %")
+ 
+// if(opration=="+"){
+//     cal=num1+num2
+// }
+// if(opration=="-"){
+//     cal=num1-num2
+// }
+// if(opration=="*"){
+//     cal=num1*num2
+// }
+// if(opration=="/"){
+//     cal=num1/num2
+// }
+// if(opration=="%"){
+//     cal=num1%num2
+// }
+
+// alert(cal)
+
